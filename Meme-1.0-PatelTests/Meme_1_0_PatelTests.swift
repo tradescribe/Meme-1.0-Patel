c74af798c178ae1a340a9fcd0053f86fad6df14f
tradescribe <harshal@tradescribe.com>
2016-04-24 15:36:10 -0400

    Initial Commit

---
+++ Meme-1.0-PatelTests/Meme_1_0_PatelTests.swift
@@ -0,0 +1,36 @@
+//
+//  Meme_1_0_PatelTests.swift
+//  Meme-1.0-PatelTests
+//
+//  Created by Harshal Patel on 4/24/16.
+//  Copyright © 2016 Harshal Patel. All rights reserved.
+//
+
+import XCTest
+@testable import Meme_1_0_Patel
+
+class Meme_1_0_PatelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
